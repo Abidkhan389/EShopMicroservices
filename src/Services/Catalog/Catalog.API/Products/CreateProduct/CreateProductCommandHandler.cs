c4be2669677f02675edf7db9c3a10d37bb5d428a
mac <abidullahkhan.se@gmail.com>
2024-04-21 17:26:56 +0500

    commit first

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Catalog.API.Products.CreateProduct;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+	:ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+internal class CreateProductCommandHandler
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    private readonly IDocumentSession _session;
+    public CreateProductCommandHandler(IDocumentSession session)
+    {
+        _session = session;
+    }
+
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name= command.Name,
+            Category= command.Category,
+            Description=command.Description,
+            ImageFile=command.ImageFile,
+            Price=command.Price
+        };
+        _session.Store(product);
+        await _session.SaveChangesAsync(cancellationToken);
+        return new CreateProductResult(product.Id);
+    }
+}
+
+
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,32 @@
+﻿using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GetProductById;
+public record GetProductByIdQuery(Guid Id): IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product Product);
+internal class GetProductByIdQueryHandler
+     //(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    private readonly IDocumentSession _session;
+    private readonly ILogger _logger;
+
+    public GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+    {
+        _session = session;
+        _logger = logger;
+    }
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+        var product = await _session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByIdResult(product);
+    }
+}
+
