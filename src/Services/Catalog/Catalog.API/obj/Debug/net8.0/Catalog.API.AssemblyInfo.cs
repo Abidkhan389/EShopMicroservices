70b9494df3d5744676c269431ee888a7e4305d31
mac <abidullahkhan.se@gmail.com>
2024-08-02 15:32:51 +0500

    Distounct service is added

--- src/Services/Catalog/Catalog.API/obj/Debug/net8.0/Catalog.API.AssemblyInfo.cs
+++ src/Services/Catalog/Catalog.API/obj/Debug/net8.0/Catalog.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+440c261f13f59d811c3380cd5f06fdb0634af1f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29cd60c84849a5943b6987d56201eeda7e8597c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
